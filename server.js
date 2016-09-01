464117e5f9ea6f41b00b62ca2a46e4ec70751d65
Austin Hudson <arhudson7@gmail.com>
2016-09-01 10:28:13 -0400

    heroku

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const path = require('path')
+const app = express()
+
+
+/* server configuration */
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+
+
+/* static route */
+app.use(express.static(__dirname + '/public'))
+
+/* config for browser history in react */
+app.get('*', (req, res) =>
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+)
+
+app.listen(process.env.PORT || 8080, () =>
+  console.log('example app listening')
+)
