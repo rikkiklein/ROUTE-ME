cc3ea970245f066b519b46582b8fe40f3b50d149
Rikki Rabinowitz <rikkirabz@gmail.com>
2016-09-06 10:03:46 -0400

    changes

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -2,20 +2,20 @@ import axios from 'axios';
 
 export default {
   getDistanceMatrix: function(data){
-    return axios.post('https://gentle-waters-71149.herokuapp.com/distance', data)
+    return axios.post('https://shielded-beyond-13922.herokuapp.com/distance', data)
 
   },
   getDirections: function(data){
-    return axios.post("https://gentle-waters-71149.herokuapp.com/directions", data)
+    return axios.post("https://shielded-beyond-13922.herokuapp.com/directions", data)
   },
   getAllRoutes: function(){
-    return axios.get('https://gentle-waters-71149.herokuapp.com/saved-routes')
+    return axios.get('https://shielded-beyond-13922.herokuapp.com/saved-routes')
   },
   saveRoutes: function(data){
-    return axios.post('https://gentle-waters-71149.herokuapp.com/saved-routes/new', data)
+    return axios.post('https://shielded-beyond-13922.herokuapp.com/saved-routes/new', data)
   },
   deleteRoute: function(name){
-    return axios.delete('https://gentle-waters-71149.herokuapp.com/saved-routes/' + name)
+    return axios.delete('https://shielded-beyond-13922.herokuapp.com/saved-routes/' + name)
   }
 
 }
